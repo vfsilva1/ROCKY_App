07a5d177f30a6590b93c12d66038d75ea96d2cd0
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2021-06-29 19:14:04 -0300

    Hamburger menu working

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //Menu hamburger
+window.onload = function () {
+    const hamburger = document.getElementById('hamburger');
+    const navList = document.getElementById('nav-list');
+    
+    hamburger.addEventListener('click', () => {
+        navList.classList.toggle('show');
+    });
+};

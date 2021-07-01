d74b936cadad1e22e62e89079790d0be9ad1ed1d
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2021-07-01 03:57:19 -0300

    Final adjustments

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function CloseMenu() {
 
 // Scroll Nav-bar
 window.onscroll = function() {navBarScroll()};
-
 function NavBarScroll() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("nav").style.position = 'fixed'

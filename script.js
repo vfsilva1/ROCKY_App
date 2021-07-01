3b6716b94ae3e1a97d6abdeefba0f76aa833c388
Vitor Ferreira da Silva <vfsilva5@gmail.com>
2021-06-30 23:37:45 -0300

    ValidateForm() added

--- script.js
+++ script.js
@@ -7,3 +7,44 @@ window.onload = function () {
         navList.classList.toggle('show');
     });
 };
+
+function ValidateForm() {
+    let name = document.forms["contactForm"]["name"];
+    let email = document.forms["contactForm"]["email"];
+    let investiment = document.forms["contactForm"]["investiment"];
+    let message = document.getElementById('messageArea');
+
+    if (name.value == "") {
+        name.style.border = '5px solid red'
+        alert('Nome precisa ser preenchido!')
+
+        return false
+    }
+    if (email.value == "") {
+        email.style.border = '5px solid red'
+        alert('E-mail precisa ser preenchido!')
+
+        return false
+    }
+    if (investiment.value < 0) {
+        investiment.style.border = '5px solid red'
+        alert('Insira um valor válido para o investimento mensal!')
+
+        return false
+    }
+    if (message.value == "") {
+        message.style.border = '5px solid red'
+        alert('Digite uma mensagem!')
+
+        return false
+    }
+
+    alert('Mensagem enviada com sucesso!')
+    return true
+}
+
+function navBarScroll() {
+    console.log('SCROLL')
+    document.getElementById("nav").style.backgroundColor = 'black';
+    document.getElementById("nav").style.width = '97%';
+}
